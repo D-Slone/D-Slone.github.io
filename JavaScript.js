7dd83cf244864dc89470eb72b323c3fa7faef0d0
D-Slone <31333447+D-Slone@users.noreply.github.com>
2017-11-14 23:24:26 -0600

    A9 changes, slideshow pictures

--- JavaScript.js
+++ JavaScript.js
@@ -99,6 +99,17 @@ function myFunction() {
     }// end window.onclick
 
 
+    var slides = document.querySelectorAll('#slides .slide');
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide,3000);
+    
+    function nextSlide() {
+        slides[currentSlide].className = 'slide';
+        currentSlide = (currentSlide+1)%slides.length;
+        slides[currentSlide].className = 'slide showing';
+    }
+
+
 
 
 var date = new Date();
